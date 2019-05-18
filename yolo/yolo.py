a9884dd765798419775a06dca2d6033b581307db
x2ever <x2ever@naver.com>
2019-05-18 20:42:54 +0900

    Update yolo.py

--- yolo/yolo.py
+++ yolo/yolo.py
@@ -17,6 +17,9 @@ import numpy as np
 import cv2
 
 from yolo.model import eval
+from utils import *
+from Box import Box
+from Tracking import Tracking
 
 from keras import backend as K
 from keras.models import load_model
@@ -93,8 +96,7 @@ class YOLO(object):
                                            iou_threshold=self.args.iou)
         return boxes, scores, classes
 
-    def detect_image(self, image):
-        start_time = timer()
+    def detect_image(self, image, tracking):
         if self.model_image_size != (None, None):
             assert self.model_image_size[
                        0] % 32 == 0, 'Multiples of 32 required'
@@ -107,7 +109,7 @@ class YOLO(object):
                               image.height - (image.height % 32))
             boxed_image = letterbox_image(image, new_image_size)
         image_data = np.array(boxed_image, dtype='float32')
-        print(image_data.shape)
+
         image_data /= 255.
         # add batch dimension
         image_data = np.expand_dims(image_data, 0)
@@ -118,33 +120,12 @@ class YOLO(object):
                 self.input_image_shape: [image.size[1], image.size[0]],
                 K.learning_phase(): 0
             })
-        print('*** Found {} face(s) for this image'.format(len(out_boxes)))
-        thickness = (image.size[0] + image.size[1]) // 400
 
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = self.class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
-            text = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
+        out_boxes = out_boxes.astype(int)
+        orders = tracking.update([Box(box[1], box[0], box[3], box[2]) for box in out_boxes])
+        image = np.asarray(image)
+        image = draw_predict_gpu(image, out_boxes, out_scores, orders)
 
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-
-            print(text, (left, top), (right, bottom))
-
-            for thk in range(thickness):
-                draw.rectangle(
-                    [left + thk, top + thk, right - thk, bottom - thk],
-                    outline=(51, 178, 255))
-            del draw
-
-        end_time = timer()
-        print('*** Processing time: {:.2f}ms'.format((end_time -
-                                                          start_time) * 1000))
         return image, out_boxes
 
     def close_session(self):
@@ -193,7 +174,6 @@ def detect_video(model, video_path=None, output=None):
 
     # the video format and fps
     # video_fourcc = int(vid.get(cv2.CAP_PROP_FOURCC))
-    video_fourcc = cv2.VideoWriter_fourcc('M', 'G', 'P', 'G')
     video_fps = vid.get(cv2.CAP_PROP_FPS)
 
     # the size of the frames to write
@@ -201,53 +181,28 @@ def detect_video(model, video_path=None, output=None):
                   int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     isOutput = True if output != "" else False
     if isOutput:
-        output_fn = 'output_video.avi'
-        out = cv2.VideoWriter(os.path.join(output, output_fn), video_fourcc, video_fps, video_size)
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        vout = cv2.VideoWriter()
+        success = vout.open('output.mp4',fourcc,video_fps,video_size,True) 
 
-    accum_time = 0
-    curr_fps = 0
-    fps = "FPS: ??"
-    prev_time = timer()
-    ###################################
+    T = Tracking()
     while True:
         ret, frame = vid.read()
         if ret:
             image = Image.fromarray(frame)
-            image, faces = model.detect_image(image)
+            image, faces = model.detect_image(image, T)
             result = np.asarray(image)
-
-            curr_time = timer()
-            exec_time = curr_time - prev_time
-            prev_time = curr_time
-            accum_time = accum_time + exec_time
-            curr_fps = curr_fps + 1
-            if accum_time > 1:
-                accum_time = accum_time - 1
-                fps = curr_fps
-                curr_fps = 0
-
-            # initialize the set of information we'll displaying on the frame
-            info = [
-                ('FPS', '{}'.format(fps)),
-                ('Faces detected', '{}'.format(len(faces)))
-            ]
-            cv2.rectangle(result, (5, 5), (120, 50), (0, 0, 0), cv2.FILLED)
-
-            for (i, (txt, val)) in enumerate(info):
-                text = '{}: {}'.format(txt, val)
-                cv2.putText(result, text, (10, (i * 20) + 20),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (10, 175, 0), 1)
 
             cv2.namedWindow("face", cv2.WINDOW_NORMAL)
             cv2.imshow("face", result)
             if isOutput:
-                out.write(result)
+                vout.write(result)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
             break
     vid.release()
-    out.release()
+    vout.release()
     cv2.destroyAllWindows()
     # close the session
     model.close_session()
